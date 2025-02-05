a42179f64c3dc8f11f93c36ff0df4bf85bffd320
Your Name <austinsong56@gmail.com>
2025-02-05 01:24:12 -0800

    ???

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -4,7 +4,7 @@ import Graph from "@/components/Graph";
 function App() {
     return (
         <div className="flex flex-col justify-between items-center min-h-screen bg-black py-6">
-            <h1 className="text-3xl font-bold">Wallet</h1>
+            <h1 className="text-3xl font-bold">Alan's Wallet</h1>
             <div className="w-full h-[60vh] md:h-[75vh] px-4">
                     <Graph />
             </div>
